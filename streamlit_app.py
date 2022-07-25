a086129c6a421e1b8b259a27301366a256c328de
guzova <dashulia.gu@gmail.com>
2022-07-25 09:55:06 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,23 +43,23 @@ try:
 except URLError as e:
   streamlit.error()
 
+streamlit.header("The fruit load list contains: ")
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+  
+if streamlit.button('Get fruit load list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlite.dataframe(my_data_rows)
 
 streamlit.stop()
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains")
-streamlit.dataframe(my_data_rows)
 
 add_new_fruit = streamlit.text_input('Select second fruit?','Apple')
 streamlit.write('Thanks for adding ', add_new_fruit)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_new_fruit)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
-
-
 
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
